4dcf960477a4f3feaa4e37475810dabcadf00bad
Ken Soh <ken@Kens-MacBook-Pro.local>
2017-01-04 13:08:41 +0700

    initial commit

---
+++ src/main/java/id/co/babe/util/App.java
@@ -0,0 +1,12 @@
+package id.co.babe.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	// default SLF4J logger used for logging execution/error messages
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) throws Exception {
+	}
+}
---
+++ src/main/java/id/co/babe/util/MySampleLibraryClass.java
@@ -0,0 +1,11 @@
+package id.co.babe.util;
+
+public class MySampleLibraryClass {
+	public void mySampleMethod1() {
+		
+	}
+	
+	public String myHelloWorld() {
+		return "Hello World!";
+	}
+}
